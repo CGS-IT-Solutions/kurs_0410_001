3c5f60f1db315d052331b7f49e3ee515fe444e49
Kurs001 <cgs.kurs001@gmail.com>
2021-10-04 16:00:07 +0200

    "2000"

--- src/main/java/cgs/TestClass.java
+++ src/main/java/cgs/TestClass.java
@@ -7,7 +7,7 @@ public class TestClass {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 2000; i++) {
             System.out.println("i = " + i);
         }
     }
