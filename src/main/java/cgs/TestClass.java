3cc9feb80eeefddc8903e3b4b69e2cbeab4989bc
Fritz Phantom <91864587+Kurs001@users.noreply.github.com>
2021-10-04 16:01:17 +0200

    what r u doing

--- src/main/java/cgs/TestClass.java
+++ src/main/java/cgs/TestClass.java
@@ -6,9 +6,8 @@ package cgs;
 public class TestClass {
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 2000; i++) {
-            System.out.println("i = " + i);
-        }
+        
+        System.out.println("... what r u doing?"); 
+        
     }
 }
