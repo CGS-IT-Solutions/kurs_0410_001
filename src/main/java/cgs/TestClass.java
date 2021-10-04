28d488135a017f0a3b41be6e34b01513eae04834
Kurs001 <cgs.kurs001@gmail.com>
2021-10-04 15:01:16 +0200

    "Maven Helper Commit"

--- src/main/java/cgs/TestClass.java
+++ src/main/java/cgs/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("args = " + args);
+            System.out.println("i = " + i);
         }
 
     }
